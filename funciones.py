44673e01b1b607df0bc395e167a5e1596938b0ab
Walaleitor <sebanicosp@gmail.com>
2017-05-06 00:45:17 -0300

    Se logro generar otra lista con la regla exactamente1, falta hacer que lo haga n veces

--- funciones.py
+++ funciones.py
@@ -9,6 +9,7 @@ def validar(lista):
             lista = []
             print ("vuelva a ingresar la lista")
             return lista
+
         else:
             print("la lista se ingreso bien")
             return lista
@@ -18,8 +19,39 @@ def validar(lista):
         return lista
 
 
+##Se logro hacer que la lista generara otra lista con la regla exactamente1
+#falta hacer que dibuje muchas mas veces
+def exactamente1(lista):
+    dibujador = []
+    size = len(lista)
+    for i in range(size):
+        if i == size -3:
+            if int(lista[size-3]) + int(lista[size-2]) + int(lista[size-1]) == 1:
+                dibujador.append(1)
+            else:
+                dibujador.append(0)
+        elif i == size -2:
+            if int(lista[size-2]) + int(lista[size-1]) + int(lista[0]) == 1:
+                dibujador.append(1)
+            else:
+                dibujador.append(0)
+        elif i == size -1:
+            if int(lista[-1]) + int(lista[0]) + int(lista[1]) == 1:
+                dibujador.append(1)
+            else:
+                dibujador.append(0)
+        else:
+            if int(lista[i]) + int(lista[i+1]) + int(lista[i+2]) == 1:
+                dibujador.append(1)
+            else:
+                dibujador.append(0)
+
+    print(dibujador)
+
+
+
 
 
 
 lista = input("ingresa lista \n")
-print(validar(lista))
+exactamente1(lista)
