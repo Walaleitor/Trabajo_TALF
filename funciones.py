d84efd6484829d829f174aff7abeced4924501ba
Walaleitor <sebanicosp@gmail.com>
2017-05-06 01:39:05 -0300

    Se genera el algoritmo de exactamente1, con numeor iteraciones ingresadas por el usuario, aun se puede mejorar

--- funciones.py
+++ funciones.py
@@ -19,34 +19,41 @@ def validar(lista):
         return lista
 
 
-##Se logro hacer que la lista generara otra lista con la regla exactamente1
+##Se logro hacer que la lista generara otra lista con la regla exactamente1,
 #falta hacer que dibuje muchas mas veces
-def exactamente1(lista):
+
+
+def exactamente1(lista,numero_de_repeticiones):
     dibujador = []
     size = len(lista)
-    for i in range(size):
-        if i == size -3:
-            if int(lista[size-3]) + int(lista[size-2]) + int(lista[size-1]) == 1:
-                dibujador.append(1)
+    print (lista)
+    for i in range(numero_de_repeticiones):
+        for i in range(size):
+            if i == size -3:
+                if int(lista[size-3]) + int(lista[size-2]) + int(lista[size-1]) == 1:
+                    dibujador.append(1)
+                else:
+                    dibujador.append(0)
+            elif i == size -2:
+                if int(lista[size-2]) + int(lista[size-1]) + int(lista[0]) == 1:
+                    dibujador.append(1)
+                else:
+                    dibujador.append(0)
+            elif i == size -1:
+                if int(lista[-1]) + int(lista[0]) + int(lista[1]) == 1:
+                    dibujador.append(1)
+                else:
+                    dibujador.append(0)
             else:
-                dibujador.append(0)
-        elif i == size -2:
-            if int(lista[size-2]) + int(lista[size-1]) + int(lista[0]) == 1:
-                dibujador.append(1)
-            else:
-                dibujador.append(0)
-        elif i == size -1:
-            if int(lista[-1]) + int(lista[0]) + int(lista[1]) == 1:
-                dibujador.append(1)
-            else:
-                dibujador.append(0)
-        else:
-            if int(lista[i]) + int(lista[i+1]) + int(lista[i+2]) == 1:
-                dibujador.append(1)
-            else:
-                dibujador.append(0)
+                if int(lista[i]) + int(lista[i+1]) + int(lista[i+2]) == 1:
+                    dibujador.append(1)
+                else:
+                    dibujador.append(0)
 
-    print(dibujador)
+        print(dibujador)
+        lista = dibujador
+        dibujador = []
+
 
 
 
@@ -54,4 +61,5 @@ def exactamente1(lista):
 
 
 lista = input("ingresa lista \n")
-exactamente1(lista)
+numero = int(input("ingresar numero de repiticiones"))
+exactamente1(lista,numero)
