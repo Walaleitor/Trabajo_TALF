e53055bbb798da9da5e1c5c4f4cbd3860ab64fe1
Walaleitor <sebanicosp@gmail.com>
2017-05-17 14:24:38 -0400

    Funcion regla de 110 añadida, falta crear menu y implementar graficos

--- funciones.py
+++ funciones.py
@@ -23,11 +23,11 @@ def validar(lista):
 #falta hacer que dibuje muchas mas veces
 
 
-def exactamente1(lista,numero_de_repeticiones):
+def  regla_exactamente1(lista):
     dibujador = []
     size = len(lista)
     print (lista)
-    for i in range(numero_de_repeticiones):
+    for i in range(50):
         for i in range(size):
             if i == size -3:
                 if int(lista[size-3]) + int(lista[size-2]) + int(lista[size-1]) == 1:
@@ -54,12 +54,59 @@ def exactamente1(lista,numero_de_repeticiones):
         lista = dibujador
         dibujador = []
 
+def regla_110(lista):
+    dibujador = []
+    size = len(lista)
+    print (lista)
+    for i in range(50):
+        for i in range(size):
+            if i == size -3:
+                if int(lista[size-3]) + int(lista[size-2]) + int(lista[size-1]) == 2:
+                    dibujador.append(1)
+                elif int(lista[size-3]) + int(lista[size-2]) + int(lista[size-1]) == 1:
+                    if lista[i] == "1":
+                        dibujador.append(0)
+                    else:
+                        dibujador.append(1)
+                else:
+                    dibujador.append(0)
+
+            elif i == size -2:
+                if int(lista[size-2]) + int(lista[size-1]) + int(lista[0]) == 2:
+                    dibujador.append(1)
+                elif int(lista[size-2]) + int(lista[size-1]) + int(lista[0]) == 1:
+                    if lista[i] == "1":
+                        dibujador.append(0)
+                    else:
+                        dibujador.append(1)
+                else:
+                    dibujador.append(0)
 
 
+            elif i == size -1:
+                if int(lista[-1]) + int(lista[0]) + int(lista[1]) == 2:
+                    dibujador.append(1)
+                elif int(lista[-1]) + int(lista[0]) + int(lista[1]) == 1:
+                    if lista[i] == "1":
+                        dibujador.append(0)
+                    else:
+                        dibujador.append(1)
+                else:
+                    dibujador.append(0)
 
 
+            else:
+                if int(lista[i]) + int(lista[i+1]) + int(lista[i+2]) == 2:
+                    dibujador.append(1)
+                elif int(lista[i]) + int(lista[i+1]) + int(lista[i+2]) == 1:
+                    if lista[i] == "1":
+                        dibujador.append(0)
+                    else:
+                        dibujador.append(1)
+                else:
+                    dibujador.append(0)
 
 
-lista = input("ingresa lista \n")
-numero = int(input("ingresar numero de repiticiones"))
-exactamente1(lista,numero)
+        print(dibujador)
+        lista = dibujador
+        dibujador = []
